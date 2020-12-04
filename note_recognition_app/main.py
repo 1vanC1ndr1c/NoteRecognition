71d93957c2d99c2603dda080736e62e026aa2568
Ivan <ivan.cindric95@gmail.com>
2020-12-04 16:49:16 +0100

    Start with data processing for the neural network input.

--- note_recognition_app/main.py
+++ note_recognition_app/main.py
@@ -1,17 +1,20 @@
 import sys
 
 from note_recognition_app.image_processing.template_matcher import extract_elements_by_template_matching
+from note_recognition_app.neural_net import prepare_the_data
 from note_recognition_app.image_processing.row_splitter import split_into_rows
 
+
 def main():
-    img_name = "img02.png"
-    #is_success = split_into_rows(img_name)
+    # img_name = "img02.png"
+    # is_success = split_into_rows(img_name)
 
     # if is_success is True:
     #     extract_elements(img_name)
 
     # extract_elements(img_name)
-    extract_elements_by_template_matching(img_name)
+    # extract_elements_by_template_matching(img_name)
+    prepare_the_data("test")
 
 
 if __name__ == '__main__':
