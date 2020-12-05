580783c889d57de2fc0fdd5b22603e558014d970
Ivan <ivan.cindric95@gmail.com>
2020-12-05 01:59:20 +0100

    Get the first version working.

--- note_recognition_app/nerual_net/main_driver.py
+++ note_recognition_app/nerual_net/main_driver.py
@@ -7,38 +7,31 @@ import tensorflow as tf
 import numpy as np
 
 
-# TODO MAKE THE DATASET A LOT SMALLER
-
 def start_neural_net():
     # Import the dataset_OLD and split it to training and testing.
     (test_data_arr, test_data_label), (train_data_arr, train_data_label) = prepare_new_data(test_data_percentage=0.2)
-    test_data_label_values = [item[0] for item in test_data_label]
-    train_data_label_values = [item[0] for item in train_data_label]
 
     # Scale these values to a range of 0 to 1 before feeding them to the neural network model
+    print("Scaling test values to [0-1] range.")
     test_data_arr = test_data_arr / 255.0
+    print("Scaling train values to [0-1] range (this will take a while).")
     train_data_arr = train_data_arr / 255.0
 
     class_names = ["A3", "A4", "A5",  # class_names contains possible results
-                   "B3", "B4",
-                   "C3", "C4",
-                   "D3", "D4",
-                   "E3", "E4",
-                   "F3", "F4",
-                   "G3", "G4",
+                   "B3", "B4", "B5",
+                   "C3", "C4", "C5",
+                   "D3", "D4", "D5",
+                   "E3", "E4", "E5",
+                   "F3", "F4", "F5",
+                   "G3", "G4", "G5",
                    "Uncategorized"]
 
-    test_data_label_values_numerical = []
-    for label_value in test_data_label_values:
-        if label_value in class_names:
-            test_data_label_values_numerical.append(class_names.index(label_value))
-    test_data_label_values_numerical = np.asarray(test_data_label_values_numerical)
-
-    train_data_label_values_numerical = []
-    for label_value in train_data_label_values:
-        if label_value in class_names:
-            train_data_label_values_numerical.append(class_names.index(label_value))
-    train_data_label_values_numerical = np.asarray(train_data_label_values_numerical)
+    # Fetch only the labels (note values) from the data.
+    test_data_label_values = [item[0] for item in test_data_label]
+    train_data_label_values = [item[0] for item in train_data_label]
+    # Assign the corresponding numerical values to labels.
+    test_data_label_values_numerical = values_to_numerical(test_data_label_values, class_names)
+    train_data_label_values_numerical = values_to_numerical(train_data_label_values, class_names)
 
     model = tf.keras.Sequential([
         tf.keras.layers.Flatten(input_shape=(200, 200)),
@@ -50,8 +43,19 @@ def start_neural_net():
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
 
-    model.fit(train_data_arr, train_data_label_values_numerical, epochs=10)
+    model.fit(train_data_arr, train_data_label_values_numerical, epochs=2)
 
     test_loss, test_acc = model.evaluate(test_data_arr, test_data_label_values_numerical, verbose=2)
 
     print('\nTest accuracy:', test_acc)
+
+
+def values_to_numerical(data_label_values, class_names):
+    data_label_values_numerical = []
+    for label_value in data_label_values:
+        if label_value in class_names:
+            data_label_values_numerical.append(class_names.index(label_value))
+        else:
+            print(label_value)
+    data_label_values_numerical = np.asarray(data_label_values_numerical)
+    return data_label_values_numerical
